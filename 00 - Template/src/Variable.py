058a4d7e10478e0c9a54dce239c406a30346fa69
Kadek Sumendra <sumendrakadek108@gmail.com>
2020-04-30 14:50:42 +0700

    type data khusus

---
+++ 00 - Template/src/Variable.py
@@ -0,0 +1,13 @@
+# variable tempat menampung data
+a = 10 
+x = 5 
+# complex type value 
+complexType = complex(5,6) 
+# mengeksekusi nilai a
+print("Nilai a =",a) 
+print("Data matematika", complexType)
+
+# type import dari bahasa c
+from ctypes import c_double 
+doubleType = c_double(10.5) 
+print("import c", doubleType)
