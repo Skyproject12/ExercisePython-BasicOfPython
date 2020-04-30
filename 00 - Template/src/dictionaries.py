3fda797e16b50faba0d25e17cd11957b7a91bdfb
Kadek Sumendra <sumendrakadek108@gmail.com>
2020-04-30 21:32:19 +0700

    hash map

---
+++ 00 - Template/src/dictionaries.py
@@ -0,0 +1,19 @@
+my_dict = {} 
+
+# add item
+my_dict['name'] = 'brian'
+my_dict['state'] = 'florida'
+my_dict['age'] = 37
+
+# accest item
+print(my_dict['name'])
+
+# change item
+my_dict['name'] = 'eginer man'
+print(my_dict['name'])
+
+# delete
+del my_dict['state'] 
+
+
+
