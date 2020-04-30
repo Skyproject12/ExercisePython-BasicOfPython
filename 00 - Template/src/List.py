6bb2872408f3b0d97290d4d3d630a6e715a28cbf
Kadek Sumendra <sumendrakadek108@gmail.com>
2020-04-30 21:02:24 +0700

    list feature

---
+++ 00 - Template/src/List.py
@@ -0,0 +1,19 @@
+my_list = [1, 2,3,":)", True, 4.5]
+
+# add the list
+my_list.append(88)
+
+# display list
+print (my_list[3])
+
+# change list
+my_list[3] = "d" 
+print (my_list[3])
+
+# delete
+del my_list[3] 
+print(my_list[3])
+
+# menampilkan semua
+for item in my_list :
+    print (item) 
